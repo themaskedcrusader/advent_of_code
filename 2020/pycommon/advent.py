6d8e0f7325ed391ad553afc2451a19d6e7225b77
Christopher Schalk <christopher.schalk@nortekcontrol.com>
2020-12-08 18:10:53 -0700

    2020: Day 7

--- 2020/pycommon/advent.py
+++ 2020/pycommon/advent.py
@@ -1,4 +1,4 @@
-import os
+import re
 
 
 def get_full_file_path(cwd, problem_key):
@@ -13,6 +13,7 @@ def read_input(cwd, problem_key):
     for line in lines:
         ret.append(line.rstrip())
     return ret
+
 
 def read_groups_as_row(cwd, problem_key):
     file = get_full_file_path(cwd, problem_key)
@@ -31,3 +32,32 @@ def read_groups_as_row(cwd, problem_key):
     # Capture the last group string and return
     ret.append(group_string)
     return ret
+
+
+bag_key = " bags contain"
+
+
+def read_baggage(cwd, problem_key):
+    file = get_full_file_path(cwd, problem_key)
+    ret = {}
+    file = open(file, 'r')
+    lines = file.readlines()
+    for line in lines:
+        outer_bag = line.split(bag_key)[0]
+        contains  = line.split(bag_key)[1]
+        contents = {}
+        for bags in contains.split(','):
+            results = re.search(" ([0-9]+) ([a-z ]+) bag", bags, re.IGNORECASE)
+            if results is not None:
+                bag = results.group(2)
+                count = results.group(1)
+                contents[bag] = count
+            else:
+                contents = None
+        ret[outer_bag] = contents
+
+    return ret
+
+
+
+
